7253f923b8436f459f1b29c66cae1b86837921fb
Jennings Anderson <jennings.anderson@colorado.edu>
2019-08-28 19:58:02 +0000

    cleaning up workshop and adding new tutorial

--- workshop_utils.py
+++ workshop_utils.py
@@ -5,28 +5,28 @@ import pandas as pd
 
 """
     This function downloads query results from the publicly accessible S3 bucket
-    and saves them to the data/ folder as <query id>.csv
+    and saves them to the /www/ folder as <query id>.csv
    
     Input: The URL of the data on AWS (from the Athena link)
     Returns: Pandas dataframe with query results 
 """
 def load_dataframe_from_s3(link):
-    base_url = "https://2019-hotsummit-aws-workshop-us2.s3.us-east-2.amazonaws.com/"
+    base_url = "https://hot-aws-workshop.s3.us-east-2.amazonaws.com/"
     if len(link)== 36:
         query_id = link
     else:
         query_id = link.split('/')[-2]
     
-    if os.path.isfile("data/"+query_id+".csv"):
+    if os.path.isfile("/www/"+query_id+".csv"):
         sys.stderr.write("Found file locally... ")
     else:
         sys.stderr.write("Downloading from S3... ")
-        with urllib.request.urlopen(base_url + query_id + ".csv") as response, open("data/"+query_id+".csv", 'wb') as out_file:
+        with urllib.request.urlopen(base_url + query_id + ".csv") as response, open("/www/"+query_id+".csv", 'wb') as out_file:
             shutil.copyfileobj(response, out_file)
-        sys.stderr.write("Query results saved to: \n"+"data/"+query_id+".csv\n")
+        sys.stderr.write("Query results saved to: \n"+"/www/"+query_id+".csv\n")
     
     sys.stderr.write("Creating dataframe... ")
-    d = pd.read_csv('data/'+query_id+".csv")
+    d = pd.read_csv('/www/'+query_id+".csv")
     sys.stderr.write("done.  Found {:,} rows".format(len(d)))
     return d
 
