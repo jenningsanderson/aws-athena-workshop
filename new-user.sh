fe5e268b59c5d312048a9bc1963de8fd83cd1884
Jennings Anderson <jennings.anderson@colorado.edu>
2019-09-19 08:09:18 +0200

    Delete new-user.sh

--- new-user.sh
+++
@@ -1,9 +0,0 @@
-echo "Creating new user: $1" 
-sudo useradd -G jupyterhub-users $1
-
-echo "Copying over workshop materials"
-sudo cp -r *.ipynb /home/$1/
-sudo cp -r *.py /home/$1/
-
-echo "Resetting permissions"
-sudo chown -R $1:jupyterhub-users /home/$1/
